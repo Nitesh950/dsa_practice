a6acaa0e17b8f844337594471532b02ffa1258f5
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-12 11:24:29 +0530

    Added solution - LeetHub

---
+++ Juggler Sequence - GFG/juggler-sequence.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            StringBuilder out = new StringBuilder();
+            ans = ob.jugglerSequence(N);
+            for(int i = 0;i < ans.size();i++)
+                out.append(ans.get(i) + " ");
+            System.out.println(out);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static List<Integer> jugglerSequence(int N){
+        // code here
+        return helper(new ArrayList<Integer>(), N);
+        
+    }
+    static List<Integer> helper(List<Integer> ans, int N){
+        if(N==1){
+            ans.add(N);
+            return ans;
+        }
+        
+        ans.add(N);
+        if(N%2==1){
+            helper(ans, (int)(Math.floor(Math.pow(N, 1.5))));
+        }else{
+            helper(ans, (int)(Math.floor(Math.pow(N, 0.5))));
+        }
+        
+        return ans;
+    }
+}
