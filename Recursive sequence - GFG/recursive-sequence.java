0f0f0b4d8254ac325a5936ff06dbddc2f3d9b392
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-16 12:55:27 +0530

    Added solution - LeetHub

---
+++ Recursive sequence - GFG/recursive-sequence.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.sequence(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long sequence(int N){
+        // code here
+        return helper(1, 1, 1, N, 0);
+    }
+    static long helper(int noOfTerms, int noOfMultiples, int startingTerm ,int n, long ans){
+        if(noOfTerms==n+1){
+            return ans;
+        }
+        long product = 1;
+        int lastTerm = 0;
+        for(int i=0; i<noOfMultiples;i++){
+             lastTerm = (i+startingTerm);
+            product *= lastTerm;
+        }
+        startingTerm = lastTerm+1;
+        ans+=product;
+        long sum = helper(noOfTerms+1, noOfMultiples+1, startingTerm, n, ans);
+        return sum;
+        
+    }
+}
