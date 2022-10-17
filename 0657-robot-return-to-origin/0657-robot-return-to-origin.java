4869e52ff545ee5cfcc10b62892c27d52d2ea0f7
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-17 09:43:49 +0530

    Time: 5 ms (96.42%), Space: 42 MB (97.58%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,22 +1,12 @@
 class Solution {
-    public boolean judgeCircle(String moves) {
-        int netUpwards= 0;
-        int netSideways = 0;
-        for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='U'){
-                netUpwards++;
-            }else if(moves.charAt(i)=='D'){
-                netUpwards--;
-            }else if(moves.charAt(i)=='L'){
-                netSideways++;
-            }else{
-                netSideways--;
-            }
+   public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char move : moves.toCharArray()) {
+            if (move == 'U') y++;
+            if (move == 'D') y--;
+            if (move == 'L') x--;
+            if (move == 'R') x++;
         }
-        if(netSideways==0 && netUpwards==0){
-            return true;
-        }else{
-            return false;
-        }
+        return x == 0 && y == 0;
     }
 }
