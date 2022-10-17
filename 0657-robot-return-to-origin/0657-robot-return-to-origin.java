77e03e0e2921e9d6da4ff060ece7be61fe96bd4c
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-17 09:44:40 +0530

    Time: 11 ms (44.42%), Space: 45.8 MB (10.62%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -3,9 +3,9 @@ class Solution {
         int x = 0, y = 0;
         for (char move : moves.toCharArray()) {
             if (move == 'U') y++;
-            if (move == 'D') y--;
-            if (move == 'L') x--;
-            if (move == 'R') x++;
+            else if (move == 'D') y--;
+            else if (move == 'L') x--;
+            else x++;
         }
         return x == 0 && y == 0;
     }
