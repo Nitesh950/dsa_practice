29a8280fc88960560b716dc86a676666cead8f30
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-28 20:16:29 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (94.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,25 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-     for(int i=digits.length-1; i>=0;i--){
-       if(digits[i]<9){
-           digits[i]++;
-           return digits;
-       }
-         digits[i] = 0;
-     }
-        digits = new int[digits.length+1];
-        digits[0] = 1;
-        return digits;
+     int[] dummy = new int[digits.length];
+        int carry = 0;
+        int i=digits.length-1;
+        while(i>=0){
+            int sum = digits[i] + carry;
+            if(i==digits.length-1){
+                sum += 1;
+            }
+            carry = sum/10;
+            sum = sum%10;
+            dummy[i] = sum;
+            i--;
+        }
+        int[] res = new int[dummy.length+1];
+        if(carry!=0){
+            res[0] = carry;
+            
+            return res;
+        }
+        
+        return dummy;
     }
 }
