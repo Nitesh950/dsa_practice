f069eeff7442637c7a729d3c535365bd1def7973
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-10-30 09:26:43 +0530

    Time: 1 ms (99.88%), Space: 41.6 MB (94.53%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,29 +1,14 @@
 class Solution {
-    public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        int count = 0;
-        for(int i=0; i<arr.length; i++){
-            if(search(arr, arr[i]*2) != -1 && search(arr, arr[i]*2) != i){
+    public boolean checkIfExist(int[] arr)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(Integer i: arr){
+            if(set.contains(i*2) || (i%2 == 0 && set.contains(i/2)))
                 return true;
-            }
+            
+            set.add(i);
         }
         return false;
-    }
-    
-     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid] > target){
-                high = mid-1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        return -1;
     }
 }
