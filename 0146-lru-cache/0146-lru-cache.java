c361c15ccb12cce7bd227f04267ab21157a5a239
Nitesh Kumar Mishra <64763106+Nitesh950@users.noreply.github.com>
2022-11-03 13:32:10 +0530

    Time: 62 ms (93.08%), Space: 118.5 MB (86.47%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,74 +1,107 @@
 class LRUCache {
-    Node head = new Node();
-    Node tail = new Node();
-    HashMap<Integer, Node> maps;
-    int capacity;
-    
-    public LRUCache(int capacity) {
-        maps = new HashMap<>(capacity);
-        this.capacity = capacity;
-        head.next = tail;
-        tail.prev = head;
-    }
-    
-    public int get(int key) {
-        int result = -1;
-        Node node = maps.get(key);
-        if(node != null){
-            result = node.val;
-            remove(node);
-            add(node);
-        }
-        return result;
-    }
-    
-    public void put(int key, int value) {
-        if(maps.containsKey(key)){
-            Node node = maps.get(key);
-            remove(node);
-            node.val = value;
-            add(node);
-        }
-        else{
-            if(capacity == maps.size()){
-                maps.remove(tail.prev.key);
-                remove(tail.prev);
-            }
-            Node node = new Node();
-            node.key = key;
-            node.val = value;
-            maps.put(key, node);
-            add(node);
-        }
-    }
-    
-    public void add(Node node){
-        Node nextNode = head.next;
-        head.next = node;
-        node.prev = head;
-        
-        node.next = nextNode;
-        nextNode.prev = node;
-    }
-    public void remove(Node node){
-        Node prevNode = node.prev;
-        Node nextNode = node.next;
-        
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
-    }
-    
-    class Node{
+    class Node{//DLL 
         int key;
-        int val;
+        int value;
+
         Node prev;
         Node next;
-    }
-}
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+        Node(int key, int value){
+            this.key= key;
+            this.value= value;
+        }
+    }
+
+    public Node[] map;//stores each node metatdata reff w.r.t to key
+    public int count, capacity;
+    public Node head, tail;
+    
+    public LRUCache(int capacity) { //all nodes are inserded b/w head and tail node 
+        
+        this.capacity= capacity;
+        count= 0;
+        
+        map= new Node[10_000+1];//no of nodes //metadata w.r.t. to key 
+        
+        head= new Node(0,0);
+        tail= new Node(0,0);
+        
+        head.next= tail;
+        tail.prev= head;
+        
+        head.prev= null;
+        tail.next= null;
+    }
+    
+    public void deleteNode(Node node){ // deleting the node in LRU cache 
+        node.prev.next= node.next;
+        node.next.prev= node.prev;       
+        
+        return;
+    }
+    
+    public void addToHead(Node node){// adding the node in front of head i.e;in LRU cache 
+        node.next= head.next;
+        node.next.prev= node;
+        node.prev= head;
+        
+        head.next= node;      
+        
+        return;
+    }
+    
+    public int get(int key) { // O(1)
+        
+        if( map[key] != null ){ //if node is present in LRU cache 
+            
+            Node node= map[key];//getting the node metadata 
+            
+            int nodeVal= node.value;
+            
+            deleteNode(node);//deleting the node from dll //O(1)
+            
+            addToHead(node);//adding to the front of head //now visited //O(1)
+            
+            return nodeVal;//returning the value of the node w.r.t to key 
+        }
+        else
+            return -1;//node not present 
+    }
+    
+    public void put(int key, int value) { // O(1)
+        
+        if(map[key] != null){//node already exists in LRU cache 
+            
+            Node node= map[key]; //getting the node metadata
+            
+            node.value= value;//changing the node value to the current value 
+            
+            deleteNode(node);//deleting the node  //O(1)
+            
+            addToHead(node);//adding node in front of head  //O(1)
+            
+        } else {//node dosent  exists in LRU cache 
+            
+            Node node= new Node(key,value);
+            
+            map[key]= node;
+            
+            if(count < capacity){ //Case 1 //space availabe 
+                count++;
+                addToHead(node);
+            } 
+            else {//case 2 //space not availabe (capacity full)
+                
+                //deleting the least recently used node, making 1 sapce availabele
+                map[tail.prev.key]= null;
+                deleteNode(tail.prev);
+                
+                //adding the node in front of head
+                addToHead(node);
+            }
+        }
+        
+        return;
+    }
+    
+}
